f718fb9b13af44bbc9abbdd161c5008f8fff469f
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-01-31 20:13:59 -0800

    Added pseudo-code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+// ====== create 10 event listeners and update the DOM in creative ways ======
+// mouseover
+elem.addEventListener('mouseover', (event) => {
+    alert('You Clicked the Sample Button!')
+})
+
+// keydown
+
+// wheel
+
+// drag / drop
+
+// load
+
+// focus
+
+// resize
+
+// scroll
+
+// select
+
+//dblclick
